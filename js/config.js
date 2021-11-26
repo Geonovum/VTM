de00b92fa731fb974b35cc92d1b0332552146b7c
Jan van Gelder <43755492+JvGldr@users.noreply.github.com>
2021-11-26 11:22:40 +0100

    Eerste Upload
    Output van word2respec geupload
    config.js aangepast, repo naam VTM gemaakt

---
+++ js/config.js
@@ -0,0 +1,32 @@
+var respecConfig =
+{
+  pubDomain: "vtm",
+  specStatus: "GN-CV",
+  specType: "AL",
+  license: "cc-by-nd",
+  publishDate: "2021-11-25",
+  authors: [
+    {
+      name: "Jan Skornsek",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/"
+    },
+    {
+      name: "Inde de Visser",
+      company: "Geonovum",
+      companyURL: "https://www/geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Jan van Gelder",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/"
+    }
+  ],
+  shortName: "vrk",
+  format: "markdown",
+  github: "https://github.com/Geonovum/VTM",
+  issueBase: "https://github.com/Geonovum/VTM/issues",
+  edDraftURI: "https://Geonovum.github.io/VTM"
+}
