131f8f429cc2577ee26ae240aa25c50e1d7c55d2
Jan van Gelder <43755492+JvGldr@users.noreply.github.com>
2021-11-29 11:17:57 +0100

    Publicatie versie 6 van het document

--- js/config.js
+++ js/config.js
@@ -1,32 +1,33 @@
 var respecConfig =
 {
-  pubDomain: "vtm",
+  pubDomain: "md",
   specStatus: "GN-CV",
   specType: "AL",
   license: "cc-by-nd",
   publishDate: "2021-11-25",
+  previousMaturity: "geen",
   authors: [
     {
       name: "Jan Skornsek",
       company: "Geonovum",
-      companyURL: "http://www.geonovum.nl/"
+      companyURL: "https://www.geonovum.nl"
     },
     {
-      name: "Inde de Visser",
+      name: "Ine de Visser",
       company: "Geonovum",
-      companyURL: "https://www/geonovum.nl/"
+      companyURL: "https://www.geonovum.nl"
     }
   ],
   editors: [
     {
       name: "Jan van Gelder",
       company: "Geonovum",
-      companyURL: "http://www.geonovum.nl/"
+      companyURL: "https://www.geonovum.nl"
     }
   ],
-  shortName: "vrk",
+  shortName: "VTM",
   format: "markdown",
-  github: "https://github.com/Geonovum/VTM",
-  issueBase: "https://github.com/Geonovum/VTM/issues",
-  edDraftURI: "https://Geonovum.github.io/VTM"
+  github: "https://github.com/Geonovum/md-VTM",
+  issueBase: "https://github.com/Geonovum/md-VTM/issues",
+  edDraftURI: "https://Geonovum.github.io/md-VTM"
 }
